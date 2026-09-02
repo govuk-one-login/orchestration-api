ef446e4bbea7be0036a997cb4f065b5f158527f9
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2026-09-02 14:11:39 +0100

    ATO-2728: Add test for cross browser no session cookies

---
+++ sis-api/src/main/java/uk/gov/di/orchestration/sis/domain/SISAuditableEvent.java
@@ -0,0 +1,12 @@
+package uk.gov.di.orchestration.sis.domain;
+
+import uk.gov.di.orchestration.shared.domain.AuditableEvent;
+
+public enum SISAuditableEvent implements AuditableEvent {
+    ORCH_SIS_UNSUCCESSFUL_AUTHORISATION_RESPONSE_RECEIVED;
+
+    @Override
+    public AuditableEvent parseFromName(String name) {
+        return valueOf(name);
+    }
+}
