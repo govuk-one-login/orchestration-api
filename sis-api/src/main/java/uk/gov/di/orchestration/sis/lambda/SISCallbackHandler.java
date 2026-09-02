govuk-one-login/authentication-api

ATO-2728: Add SIS validation (ipv redirect)

sis-api/src/main/java/uk/gov/di/orchestration/sis/lambda/SISCallbackHandler.java
@@ -9,6 +9,8 @@ import com.nimbusds.openid.connect.sdk.AuthenticationRequest;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.ThreadContext;
+import uk.gov.di.authentication.ipv.services.InitiateIPVAuthorisationService;
+import uk.gov.di.orchestration.audit.AuditContext;
 import uk.gov.di.orchestration.audit.TxmaAuditUser;
 import uk.gov.di.orchestration.identity.entity.CrossBrowserNoSessionException;
 import uk.gov.di.orchestration.identity.entity.CrossBrowserStateMismatchException;
@@ -16,12 +18,20 @@ import uk.gov.di.orchestration.identity.entity.IdentityContext;
 import uk.gov.di.orchestration.identity.exceptions.IdentityCallbackException;
 import uk.gov.di.orchestration.identity.helpers.IdentityCallbackHelper;
 import uk.gov.di.orchestration.identity.service.IdentityContextService;
+import uk.gov.di.orchestration.shared.entity.VectorOfTrust;
 import uk.gov.di.orchestration.shared.exceptions.NoSessionException;
+import uk.gov.di.orchestration.shared.helpers.IpAddressHelper;
+import uk.gov.di.orchestration.shared.helpers.PersistentIdHelper;
 import uk.gov.di.orchestration.shared.services.AuditService;
 import uk.gov.di.orchestration.shared.services.ConfigurationService;
 import uk.gov.di.orchestration.shared.services.EndOfJourneyService;
+import uk.gov.di.orchestration.sis.service.SISAuthorisationService;
+
+import java.util.Objects;
+import java.util.Optional;
 
 import static uk.gov.di.orchestration.shared.helpers.AuditHelper.attachTxmaAuditFieldFromHeaders;
+import static uk.gov.di.orchestration.shared.helpers.InstrumentationHelper.segmentedFunctionCall;
 import static uk.gov.di.orchestration.shared.helpers.LogLineHelper.LogFieldName.AWS_REQUEST_ID;
 import static uk.gov.di.orchestration.shared.helpers.LogLineHelper.LogFieldName.CLIENT_ID;
 import static uk.gov.di.orchestration.shared.helpers.LogLineHelper.attachLogFieldToLogs;
@@ -36,18 +46,24 @@ public class SISCallbackHandler
     private final IdentityContextService identityContextService;
     private final AuditService auditService;
     private final EndOfJourneyService endOfJourneyService;
+    private final SISAuthorisationService sisAuthorisationService;
+    private final InitiateIPVAuthorisationService ipvAuthorisationService;
 
     public SISCallbackHandler(
             ConfigurationService configurationService,
             IdentityCallbackHelper identityCallbackHelper,
             IdentityContextService identityContextService,
             AuditService auditService,
-            EndOfJourneyService endOfJourneyService) {
+            EndOfJourneyService endOfJourneyService,
+            SISAuthorisationService sisAuthorisationService,
+            InitiateIPVAuthorisationService ipvAuthorisationService) {
         this.configurationService = configurationService;
         this.identityCallbackHelper = identityCallbackHelper;
         this.identityContextService = identityContextService;
         this.auditService = auditService;
         this.endOfJourneyService = endOfJourneyService;
+        this.sisAuthorisationService = sisAuthorisationService;
+        this.ipvAuthorisationService = ipvAuthorisationService;
     }
 
     @Override
@@ -66,6 +82,49 @@ public class SISCallbackHandler
             var identityContextResponse = getIdentityContext(input);
             if (identityContextResponse.earlyRedirect != null) {
                 return identityContextResponse.earlyRedirect;
+            }
+            var identityContext = identityContextResponse.identityContext;
+            var orchSession = identityContext.orchSessionItem();
+            var sessionId = orchSession.getSessionId();
+            var orchClientSession = identityContext.orchClientSessionItem();
+            var clientSessionId = orchClientSession.getClientSessionId();
+            var clientRegistry = identityContext.clientRegistry();
+            var clientId = clientRegistry.getClientID();
+            var authUserInfo = identityContext.authUserInfo();
+            var authRequest = identityContext.authRequest();
+
+            var persistentId =
+                    PersistentIdHelper.extractPersistentIdFromCookieHeader(input.getHeaders());
+            var ipAddress = IpAddressHelper.extractIpAddress(input);
+            var auditContext =
+                    new AuditContext(
+                            clientSessionId,
+                            sessionId,
+                            clientId,
+                            orchSession.getInternalCommonSubjectId(),
+                            authUserInfo.getEmailAddress(),
+                            ipAddress,
+                            Objects.isNull(authUserInfo.getPhoneNumber())
+                                    ? AuditService.UNKNOWN
+                                    : authUserInfo.getPhoneNumber(),
+                            persistentId);
+
+            var user =
+                    TxmaAuditUser.user()
+                            .withGovukSigninJourneyId(clientSessionId)
+                            .withSessionId(sessionId)
+                            .withUserId(orchSession.getInternalCommonSubjectId())
+                            .withEmail(authUserInfo.getEmailAddress())
+                            .withPhone(
+                                    Objects.isNull(authUserInfo.getPhoneNumber())
+                                            ? AuditService.UNKNOWN
+                                            : authUserInfo.getPhoneNumber())
+                            .withPersistentSessionId(persistentId);
+
+            var validationRedirectOpt =
+                    validateAuthResponse(input, identityContext, auditContext, user);
+            if (validationRedirectOpt.isPresent()) {
+                return validationRedirectOpt.get();
             }
         } catch (IdentityCallbackException e) {
             return identityCallbackHelper.redirectToFrontendErrorPageWithErrorLog(e);
@@ -105,4 +164,43 @@ public class SISCallbackHandler
 
     private record IdentityContextResponse(
             IdentityContext identityContext, APIGatewayProxyResponseEvent earlyRedirect) {}
+
+    private Optional<APIGatewayProxyResponseEvent> validateAuthResponse(
+            APIGatewayProxyRequestEvent input,
+            IdentityContext identityContext,
+            AuditContext auditContext,
+            TxmaAuditUser user) {
+        var validationErrorOpt =
+                segmentedFunctionCall(
+                        "validateSISAuthResponse",
+                        () ->
+                                sisAuthorisationService.validateResponse(
+                                        input.getQueryStringParameters(),
+                                        identityContext.orchSessionItem().getSessionId()));
+
+        if (validationErrorOpt.isPresent()) {
+            auditService.submitAuditEvent(
+                    ORCH_SIS_UNSUCCESSFUL_AUTHORISATION_RESPONSE_RECEIVED,
+                    identityContext.clientRegistry().getClientID(),
+                    user);
+            var validationError = validationErrorOpt.get();
+            if (validationError.userShouldRouteToIpv()) {
+                return Optional.ofNullable(
+                        ipvAuthorisationService.sendRequestToIPV(
+                                input,
+                                identityContext.authRequest(),
+                                identityContext.authUserInfo(),
+                                identityContext.orchSessionItem().getSessionId(),
+                                identityContext.clientRegistry(),
+                                identityContext.clientRegistry().getClientID(),
+                                identityContext.orchClientSessionItem().getClientSessionId(),
+                                auditContext.persistentSessionId(),
+                                false,
+                                VectorOfTrust.getRequestedLevelsOfConfidence(
+                                        identityContext.orchClientSessionItem().getVtrList()),
+                                validationError.userRequestedUpdate()));
+            }
+        }
+        return Optional.empty();
+    }
 }

govuk-one-login/orchestration-api

sis-api/src/main/java/uk/gov/di/orchestration/sis/lambda/SISCallbackHandler.java
@@ -9,6 +9,8 @@ import com.nimbusds.openid.connect.sdk.AuthenticationRequest;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.ThreadContext;
+import uk.gov.di.authentication.ipv.services.InitiateIPVAuthorisationService;
+import uk.gov.di.orchestration.audit.AuditContext;
 import uk.gov.di.orchestration.audit.TxmaAuditUser;
 import uk.gov.di.orchestration.identity.entity.CrossBrowserNoSessionException;
 import uk.gov.di.orchestration.identity.entity.CrossBrowserStateMismatchException;
@@ -16,12 +18,21 @@ import uk.gov.di.orchestration.identity.entity.IdentityContext;
 import uk.gov.di.orchestration.identity.exceptions.IdentityCallbackException;
 import uk.gov.di.orchestration.identity.helpers.IdentityCallbackHelper;
 import uk.gov.di.orchestration.identity.service.IdentityContextService;
+import uk.gov.di.orchestration.shared.entity.VectorOfTrust;
 import uk.gov.di.orchestration.shared.exceptions.NoSessionException;
+import uk.gov.di.orchestration.shared.helpers.IpAddressHelper;
+import uk.gov.di.orchestration.shared.helpers.PersistentIdHelper;
+import uk.gov.di.orchestration.shared.oauth.OAuthService;
 import uk.gov.di.orchestration.shared.services.AuditService;
 import uk.gov.di.orchestration.shared.services.ConfigurationService;
 import uk.gov.di.orchestration.shared.services.EndOfJourneyService;
+import uk.gov.di.orchestration.sis.exception.SISCallbackValidationError;
+
+import java.util.Objects;
+import java.util.Optional;
 
 import static uk.gov.di.orchestration.shared.helpers.AuditHelper.attachTxmaAuditFieldFromHeaders;
+import static uk.gov.di.orchestration.shared.helpers.InstrumentationHelper.segmentedFunctionCall;
 import static uk.gov.di.orchestration.shared.helpers.LogLineHelper.LogFieldName.AWS_REQUEST_ID;
 import static uk.gov.di.orchestration.shared.helpers.LogLineHelper.LogFieldName.CLIENT_ID;
 import static uk.gov.di.orchestration.shared.helpers.LogLineHelper.attachIpAddressAndUserAgentToLogs;
@@ -37,18 +48,24 @@ public class SISCallbackHandler
     private final IdentityContextService identityContextService;
     private final AuditService auditService;
     private final EndOfJourneyService endOfJourneyService;
+    private final OAuthService sisAuthorisationService;
+    private final InitiateIPVAuthorisationService ipvAuthorisationService;
 
     public SISCallbackHandler(
             ConfigurationService configurationService,
             IdentityCallbackHelper identityCallbackHelper,
             IdentityContextService identityContextService,
             AuditService auditService,
-            EndOfJourneyService endOfJourneyService) {
+            EndOfJourneyService endOfJourneyService,
+            OAuthService sisAuthorisationService,
+            InitiateIPVAuthorisationService ipvAuthorisationService) {
         this.configurationService = configurationService;
         this.identityCallbackHelper = identityCallbackHelper;
         this.identityContextService = identityContextService;
         this.auditService = auditService;
         this.endOfJourneyService = endOfJourneyService;
+        this.sisAuthorisationService = sisAuthorisationService;
+        this.ipvAuthorisationService = ipvAuthorisationService;
     }
 
     @Override
@@ -68,6 +85,49 @@ public class SISCallbackHandler
             var identityContextResponse = getIdentityContext(input);
             if (identityContextResponse.earlyRedirect != null) {
                 return identityContextResponse.earlyRedirect;
+            }
+            var identityContext = identityContextResponse.identityContext;
+            var orchSession = identityContext.orchSessionItem();
+            var sessionId = orchSession.getSessionId();
+            var orchClientSession = identityContext.orchClientSessionItem();
+            var clientSessionId = orchClientSession.getClientSessionId();
+            var clientRegistry = identityContext.clientRegistry();
+            var clientId = clientRegistry.getClientID();
+            var authUserInfo = identityContext.authUserInfo();
+            var authRequest = identityContext.authRequest();
+
+            var persistentId =
+                    PersistentIdHelper.extractPersistentIdFromCookieHeader(input.getHeaders());
+            var ipAddress = IpAddressHelper.extractIpAddress(input);
+            var auditContext =
+                    new AuditContext(
+                            clientSessionId,
+                            sessionId,
+                            clientId,
+                            orchSession.getInternalCommonSubjectId(),
+                            authUserInfo.getEmailAddress(),
+                            ipAddress,
+                            Objects.isNull(authUserInfo.getPhoneNumber())
+                                    ? AuditService.UNKNOWN
+                                    : authUserInfo.getPhoneNumber(),
+                            persistentId);
+
+            var user =
+                    TxmaAuditUser.user()
+                            .withGovukSigninJourneyId(clientSessionId)
+                            .withSessionId(sessionId)
+                            .withUserId(orchSession.getInternalCommonSubjectId())
+                            .withEmail(authUserInfo.getEmailAddress())
+                            .withPhone(
+                                    Objects.isNull(authUserInfo.getPhoneNumber())
+                                            ? AuditService.UNKNOWN
+                                            : authUserInfo.getPhoneNumber())
+                            .withPersistentSessionId(persistentId);
+
+            var validationRedirectOpt =
+                    validateAuthResponse(input, identityContext, auditContext, user);
+            if (validationRedirectOpt.isPresent()) {
+                return validationRedirectOpt.get();
             }
         } catch (IdentityCallbackException e) {
             return identityCallbackHelper.redirectToFrontendErrorPageWithErrorLog(e);
@@ -107,4 +167,43 @@ public class SISCallbackHandler
 
     private record IdentityContextResponse(
             IdentityContext identityContext, APIGatewayProxyResponseEvent earlyRedirect) {}
+
+    private Optional<APIGatewayProxyResponseEvent> validateAuthResponse(
+            APIGatewayProxyRequestEvent input,
+            IdentityContext identityContext,
+            AuditContext auditContext,
+            TxmaAuditUser user) {
+        var validationErrorOpt =
+                segmentedFunctionCall(
+                        "validateSISAuthResponse",
+                        () ->
+                                sisAuthorisationService.validateCallback(
+                                        input.getQueryStringParameters(),
+                                        identityContext.orchSessionItem().getSessionId()));
+
+        if (validationErrorOpt.isPresent()) {
+            auditService.submitAuditEvent(
+                    ORCH_SIS_UNSUCCESSFUL_AUTHORISATION_RESPONSE_RECEIVED,
+                    identityContext.clientRegistry().getClientID(),
+                    user);
+            var validationError = (SISCallbackValidationError) validationErrorOpt.get();
+            if (validationError.userShouldRouteToIpv()) {
+                return Optional.ofNullable(
+                        ipvAuthorisationService.sendRequestToIPV(
+                                input,
+                                identityContext.authRequest(),
+                                identityContext.authUserInfo(),
+                                identityContext.orchSessionItem().getSessionId(),
+                                identityContext.clientRegistry(),
+                                identityContext.clientRegistry().getClientID(),
+                                identityContext.orchClientSessionItem().getClientSessionId(),
+                                auditContext.persistentSessionId(),
+                                false,
+                                VectorOfTrust.getRequestedLevelsOfConfidence(
+                                        identityContext.orchClientSessionItem().getVtrList()),
+                                validationError.userRequestedUpdate()));
+            }
+        }
+        return Optional.empty();
+    }
 }